ad3fdb0ab20b75c43ebe05064cf25ff9bf2f0e8e
김진규 <zamong99@gmail.com>
2023-10-09 00:35:15 +0900

    jest 셋업

---
+++ test/rpc.test.ts
@@ -0,0 +1,8 @@
+import magnet from 'magnet-uri';
+import rpc from '../src/Rpc';
+
+describe('description', () => {
+  it('testcase', () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ test/sample.test.ts
@@ -0,0 +1,5 @@
+describe('description', () => {
+  it('testcase', () => {
+    expect(1).toBe(1);
+  });
+});
