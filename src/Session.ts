4d14e875f189c9fe85af773271f9f0a08443fea6
김진규 <zamong99@gmail.com>
2023-10-16 09:53:56 +0900

    세션 업데이트

--- src/Session.ts
+++ src/Session.ts
@@ -1,4 +1,30 @@
-class Session {
-  constructor() {}
+import { EventEmitter } from "events";
+
+interface Options {
+  timeout;
+}
+
+class Session extends EventEmitter {
+
+  timeout;
+
+  timer;
+  destroyed;
+
+  constructor(opts) {
+    super();
+    this.timeout = opts.timeout || 2000;
+    this.timer = setInterval(this.onTick, Math.floor(this.timeout / 4))
+  }
+
+  onTick() {
+
+  }
+
+  destroy(cb: Function) {
+    this.destroyed = true;
+    clearInterval(this.timer);
+  }
+
 }
 module.exports = Session;
