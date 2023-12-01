49d77e3772ee4fdf03f9e205bf7616203dd2123e
김진규 <zamong99@gmail.com>
2023-12-01 23:36:57 +0900

    Cmd 클래스로 변경
    오리지널 버전과는 다르게 난 좀 더 완벽한 oop 를 원한다

--- src/Cmd.ts
+++ src/Cmd.ts
@@ -6,126 +6,151 @@ import stripIndent from 'common-tags/lib/stripIndent/index.js'
 import Client from './Client'
 import Torrent from './Torrent';
 
-const options = {
-  advanced: {
-    q: { alias: 'quiet', desc: 'Don\'t show UI on stdout', type: 'boolean' },
-  }
-}
-
-const commands = [
-  { command: ['download [torrent-ids...]', '$0'], desc: 'Download a torrent', handler: (args) => { processInputs(args.torrentIds, runDownload) } },
-  { command: 'downloadmeta <torrent-ids...>', desc: 'Download metadata of torrent', handler: (args) => { processInputs(args.torrentIds, runDownloadMeta) } },
-  { command: 'help', desc: 'Show help information', handler: null }
-];
-
-let server, client, argv;
-let gracefullyExiting = false;
-const yargs = Yargs();
-
-function exec() {
-  process.title = 'torrent-ts';
-  process.on('exit', code => {
-    console.log('exit event is called...');
-    if(code === 0) return; // normal exit
-    if(code === 130) return; // intentional exit with Control-C
-  });
-  process.on('SIGINT', gracefulExit);
-  process.on('SIGTERM', gracefulExit);
-
-  yargs
-    .scriptName('torrent-ts')
-    .usage(
-      stripIndent`
-        Usage:
-          torrent-ts [command] <torrent-id> [options]
-      `);
-  yargs.command(commands);
-  yargs.middleware(init)
-  yargs
-    .strict()
-    .help('help', 'Show help information')
-    .alias({ help: 'h', version: 'v' })
-    .parse(hideBin(process.argv), { startTime: Date.now() })
-}
-
-function gracefulExit() {
-  if(gracefullyExiting)
-    return;
+export default class Cmd {
   
-  gracefullyExiting = true;
+  server;
+  client;
+  argv;
+  gracefullyExiting = false;
 
-  console.log(chalk`\n{green webtorrent is exiting...}`);
-
-  process.removeListener('SIGINT', gracefulExit);
-  process.removeListener('SIGTERM', gracefulExit);
-
-  if(!client)
-    return;
+  yargs = Yargs();
   
-  client.destroy(err => {
-    if(err) {
-      return fatalError(err);
+  constructor() {}
+
+  exec() {
+    process.title = 'torrent-ts';
+    process.on('exit', code => {
+      console.log('exit event is called...');
+      if(code === 0) return; // normal exit
+      if(code === 130) return; // intentional exit with Control-C
+    });
+    process.on('SIGINT', this.gracefulExit.bind(this));
+    process.on('SIGTERM', this.gracefulExit.bind(this));
+  
+    const options = {
+      advanced: {
+        q: { alias: 'quiet', desc: 'Don\'t show UI on stdout', type: 'boolean' },
+      }
     }
-  });
-}
+    
+    const commands = [
+      { command: ['download [torrent-ids...]', '$0'], desc: 'Download a torrent', handler: (args) => { this.processInputs(args.torrentIds, this.runDownload.bind(this)) } },
+      { command: 'downloadmeta <torrent-ids...>', desc: 'Download metadata of torrent', handler: (args) => { this.processInputs(args.torrentIds, this.runDownloadMeta.bind(this)) } },
+      { command: 'help', desc: 'Show help information', handler: null }
+    ];
 
-function fatalError(err) {
-  console.log(chalk`{red Error:} ${err.message || err}`);
-  process.exit(1);
-}
-
-function init(_argv) {
-  // console.log('init() is called...');
-  argv = _argv;
-}
-
-function processInputs(inputs, fn) {
-  // console.log('processInputs() is called...');
-  if(Array.isArray(inputs) && inputs.length !== 0) {
-    inputs.forEach(input => fn(input));
-  } else {
-    yargs.showHelp('log');
+    this.yargs
+      .scriptName('torrent-ts')
+      .usage(
+        stripIndent`
+          Usage:
+            torrent-ts [command] <torrent-id> [options]
+        `);
+    this.yargs.command(commands);
+    this.yargs.middleware(this.init.bind(this))
+    this.yargs
+      .strict()
+      .help('help', 'Show help information')
+      .alias({ help: 'h', version: 'v' })
+      .parse(hideBin(process.argv), { startTime: Date.now() })
   }
-}
-
-function runDownload(torrentId) {
-  // console.log('runDownload() is called...');
-  client = new Client({});
-  let torrent = client.add(torrentId);
-  torrent.on('infoHash', () => {});
-  torrent.on('metadata', () => {});
-  torrent.on('done', () => {});
-  torrent.startDiscovery();
-}
-
-function runDownloadMeta(torrentId) {
-  // console.log('argv.out =', argv.out, ', argv.quiet =', argv.quiet);
-  client = new Client({});
-  const torrent = client.add(torrentId);
-  torrent.on('infoHash', function() {
-    const torrentFilePath = `${argv.out}/${this.infoHash}.torrent`;
-
-    if(argv.quiet)
+  
+  /**
+   * must be called with binding for this
+   * @returns 
+   */
+  gracefulExit() {
+    if(this.gracefullyExiting)
       return;
     
-    updateMetadata();
-    torrent.on('wire', updateMetadata);
-
-    function updateMetadata() {
-      console.clear();
-      console.log(chalk`{green fetching torrent metadata from} {bold ${torrent.numPeers}} {green peers}`);
+      this.gracefullyExiting = true;
+  
+    console.log(chalk`\n{green webtorrent is exiting...}`);
+  
+    process.removeListener('SIGINT', this.gracefulExit);
+    process.removeListener('SIGTERM', this.gracefulExit);
+  
+    if(!this.client)
+      return;
+    
+    this.client.destroy(err => {
+      if(err) {
+        return this.fatalError(err);
+      }
+    });
+  }
+  
+  fatalError(err) {
+    console.log(chalk`{red Error:} ${err.message || err}`);
+    process.exit(1);
+  }
+  
+  /**
+   * must be called with binding for this
+   * @param _argv 
+   */
+  init(_argv) {
+    console.log('init() is called...');
+    this.argv = _argv;
+  }
+  
+  processInputs(inputs, fn) {
+    console.log('processInputs() is called...');
+    if(Array.isArray(inputs) && inputs.length !== 0) {
+      inputs.forEach(input => fn(input));
+    } else {
+      this.yargs.showHelp('log');
     }
-
-    torrent.on('metadata', function () {
-      console.clear();
-      torrent.removeListener('wire', updateMetadata);
-
-      console.clear();
-      console.log(chalk`{green saving the .torrent file data to ${torrentFilePath} ...}`);
-      fs.writeFileSync(torrentFilePath, this.torrentFile);
-      gracefulExit();
-    })
-  });
+  }
+  
+  /**
+   * must be called with binding for this
+   * @param torrentId 
+   */
+  runDownload(torrentId) {
+    // console.log('runDownload() is called...');
+    this.client = new Client({});
+    const torrent = this.client.add(torrentId);
+    torrent.on('infoHash', () => {});
+    torrent.on('metadata', () => {});
+    torrent.on('done', () => {});
+    // torrent.startDiscovery();
+  }
+  
+  /**
+   * must be called with binding for this
+   * @param torrentId 
+   */
+  runDownloadMeta(torrentId) {
+    // console.log('argv.out =', argv.out, ', argv.quiet =', argv.quiet);
+    let self = this;
+    this.client = new Client({});
+    const torrent = this.client.add(torrentId);
+    torrent.on('infoHash', function() {
+      const torrentFilePath = `${this.argv.out}/${this.infoHash}.torrent`;
+  
+      // if(this.argv.quiet)
+      //   return;
+      
+      updateMetadata();
+      torrent.on('wire', updateMetadata);
+  
+      function updateMetadata() {
+        // console.clear();
+        console.log(chalk`{green fetching torrent metadata from} {bold ${torrent.numPeers}} {green peers}`);
+      }
+  
+      torrent.on('metadata', function() {
+        // console.clear();
+        torrent.removeListener('wire', updateMetadata);
+  
+        // console.clear();
+        console.log(chalk`{green saving the .torrent file data to ${torrentFilePath} ...}`);
+        fs.writeFileSync(torrentFilePath, this.torrentFile);
+        self.gracefulExit();
+      })
+    });
+  }
 }
 
-exec();
+new Cmd().exec();
--- test/cmd.test.ts
+++ test/cmd.test.ts
@@ -2,7 +2,7 @@ import Cmd from '../src/Cmd';
 
 describe('description', () => {
   it('testcase', () => {
-    new Cmd().exec([]);
+    new Cmd().exec();
     expect(1).toBe(1);
   });
 });
